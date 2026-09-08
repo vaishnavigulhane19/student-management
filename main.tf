82d46b652decdc242fd10d4f6acc9ce89d0a9754
vaishnavigulhane19 <vaishnavigulhane530@gmail.com>
2026-09-08 23:38:11 +0530

    Update S3 bucket and AWS region in main.tf

--- main.tf
+++ main.tf
@@ -11,15 +11,15 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "mayurcbz.space"
+    bucket       = "deploywithnavi.space"
     key          = "student-management/eks/terraform.tfstate"
-    region       = "us-west-2"
+    region       = "ap-south-1"
     use_lockfile = true
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 # Fetch default VPC
